af28559388fa280e2463bedf6e06f2fa6a69478a
GyebnarPeter <gyebnarpetter@gmail.com>
2021-03-24 17:18:10 +0100

    complete task, one test failed

--- src/main/java/com/codecool/database/RadioCharts.java
+++ src/main/java/com/codecool/database/RadioCharts.java
@@ -1,6 +1,43 @@
 package com.codecool.database;
 
+import java.sql.*;
 
 public class RadioCharts {
-    //TODO: implement this class
+
+    private final String URL;
+    private final String USER;
+    private final String PASSWORD;
+
+    public RadioCharts(String url, String user, String password) {
+        this.URL = url;
+        this.USER = user;
+        this.PASSWORD = password;
+    }
+
+    public String getMostPlayedSong() {
+        String sql = "SELECT song FROM music_broadcast GROUP BY song ORDER BY SUM(times_aired) DESC LIMIT 1;";
+        try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD)) {
+            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            resultSet.next();
+            return resultSet.getString(1);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return "";
+    }
+
+    public String getMostActiveArtist() {
+        String sql = "SELECT artist FROM music_broadcast GROUP BY artist ORDER BY COUNT(DISTINCT song) DESC LIMIT 1;";
+        try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD)) {
+            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            resultSet.next();
+            return resultSet.getString(1);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return "";
+    }
+
 }
